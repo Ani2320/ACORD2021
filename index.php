3243e216150198aca7d27637849b7b06dc0a1a70
Ani2320 <sargsyanani01@gmail.com>
2021-08-05 01:37:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once("consultas/coneccion.php");
+require_once("include/funciones.php");
+require_once("include/1head.php");
+require_once("include/2header.php");
+require_once("include/carrusel.php");
+require_once("include/form_registro_usuario.php");
+require_once("include/form_ingreso_usuario.php");
+mostrar_productos($con);
+?>
+
+
+
+<?php require_once("include/footer.php");  ?>
+
+</body>
+
+</html>
